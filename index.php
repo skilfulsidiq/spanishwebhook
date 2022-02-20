569d62af4b28f62c1a5cbb8bd811ea1bf5b40f26
skilfulsidiq <osodiq@gmail.com>
2022-02-20 09:38:34 +0100

    make the controller return

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 // Our web handlers
-var_dump("hello");
+
 $app->get('/', function() use($app) {
   
 header('Content-Type: application/json');
@@ -32,7 +32,7 @@ if($json = json_decode(file_get_contents("php://input"), true)){
 // $answers = $data->form_response->answers;
 // print_r($answers);
 header('Content-Type: application/json; charset=utf-8');
-$answers= $data->form_response->answers;
+// $answers= $data->form_response->answers;
 return json_encode($data);
   // $app['monolog']->addDebug('logging output.');
   // return $app['twig']->render('index.twig');
